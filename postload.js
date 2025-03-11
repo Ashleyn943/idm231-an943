596421f8a00a6422abc6974b22ffcaffb1bb11be
Ashley <an943@drexel.edu>
2025-03-11 19:10:32 -0400

    rearrange code to eliminate loading issues

--- postload.js
+++ postload.js
@@ -66,100 +66,100 @@ function displayOInfo(whichOne) {
             ZodPicObj.src = 'Media/Cinnamoroll.png';
             ZodDatesObj.innerHTML = 'August 23 - September 22';
             ZodDesObj.innerHTML = 'In social settings, you often stick to the walls and stay quiet. Not by choice, but simply because you\’re so shy. You definitely have a tough time speaking up in most situations. However, that doesn\’t mean you don\’t want to be around people at all. You adore your friends and being around them brings you insurmountable joy.';
+            //plays audio 
+            playAudio('CinnaAudio');
             //makes overlay appear 
             DisplayZod.classList.remove('hideMe');
             //blurs background
             BackgroundDisplay.classList.add('ZodChara');
-            //plays audio 
-            playAudio('CinnaAudio');
             break;
         case 'Pompompurin':
             ZodPicObj.src = 'Media/Pompompurin.png';
             ZodDatesObj.innerHTML = 'December 22 - January 19';
             ZodDesObj.innerHTML = 'Out of all your friends, you have the most energy. Sometimes it\’s for exercise, sometimes it\’s for work. Either way, you put 100% into what you love. You\’re also the one who makes plans and gathers everyone to bring those plans to reality. For you, nothing beats a day out with those you love. Well, a nap at the end of such a fulfilling day might be better.';
+            playAudio('PomAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('PomAudio');
             break;
         case 'Tuxedo Sam':
             ZodPicObj.src = 'Media/Tuxedo_Sam.png';
             ZodDatesObj.innerHTML = 'April 20 - May 20';
             ZodDesObj.innerHTML = 'Sometimes, you\’re called the mom friend. You love to eat delicious food and by extension, feed those around you. You also love to neaten things up. From clothing to rooms, you love when things are absolutely perfect. It even irritates you to walk into a messy room or notice a hair out of place. If your friend is the owner the mess, you\’ll help them clean up for both your sake and theirs. Just like any parent, you can also be very stubborn about matters. You always have the best intentions even if you\'re stuck on one approach.';
+            playAudio('SamAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('SamAudio');
             break;
         case 'My Melody':
             ZodPicObj.src = 'Media/My_Melody.png';
             ZodDatesObj.innerHTML = 'September 23 - October 22';
             ZodDesObj.innerHTML = 'From an outside perspective, you\’re sweet, kind, caring, and a bit of a klutz. You\’re also honest to a fault but only because you care about your friends so deeply that you don\’t want to hide the truth from them. Even though you\’re considered naive due to your childish outward appearance, you have a large understanding of emotions to care for your friends.';
+            playAudio('MeloAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('MeloAudio');
             break;
         case 'Keroppi':
             ZodPicObj.src = 'Media/Keroppi.png';
             ZodDatesObj.innerHTML = 'July 23 - August 22';
             ZodDesObj.innerHTML = 'An extrovert through and through. Anything to be around people, you love to do it. Especially if it involves games. You love playing all kinds of games with your friends. From board games to ball games to even video games at times, you love games. You get so excited about games that you can become careless. For you, it\’s not about winning the game but having fun. It might feel bad to lose but you don\’t let it get to you and bounce back immediately.';
+            playAudio('KeroAudio'); 
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('KeroAudio');
             break;
         case 'Chococat':
             ZodPicObj.src = 'Media/Chococat.png';
             ZodDatesObj.innerHTML = 'November 22 – December 21';
             ZodDesObj.innerHTML = 'Most people are curious and like to learn about new things. For some people, these new things can grab their attention and keep them thinking about it all the time. You happen to do just that. You\’ll try to gather and learn everything about this new interest before you finally leave the state of fixation you\’ve put yourself in. While learning is everything to you, your thoughts also jump around a lot. It concerns those close to you but as long as you take care of yourself, there should be nothing to be concerned about.';
+            playAudio('ChocoAudio');  
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('ChocoAudio');
             break;
         case 'Badtz maru':
             ZodPicObj.src = 'Media/Badtz_maru.png';
             ZodDatesObj.innerHTML = 'March 21 - April 19';
             ZodDesObj.innerHTML = 'People often call you childish for the decisions you make but in reality, you just love having fun. In any discussion, you love to play devil\’s advocate just to see others get riled up. For similar reasons, you like to tease others whenever the chance is brought up. While it\’s selfish to prioritize your fun over other people\’s feelings, you always make sure no one is actually hurt in the end. You never want to actually hurt anyone, you just want to play the bad guy for fun.';
+            playAudio('BadAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('BadAudio');
             break;
         case 'Hangyodon':
             ZodPicObj.src = 'Media/Hangyodon.png';
             ZodDatesObj.innerHTML = 'October 23 – November 21';
             ZodDesObj.innerHTML = 'Some people call you a clown but you don\’t mind it. In fact, you love making people laugh. You love playing pranks and trying to make people smile. If they don\’t go quite right, you will make things right even at your own expense. On a similar note, you love helping others out. Anyway you can be a hero, especially for your loved one, you want to do it. This heroism might be the reason you do everything you can to make sure everyone is smiling.';
+            playAudio('HangAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('HangAudio');
             break;
         case 'Kuromi':
             ZodPicObj.src = 'Media/Kuromi.png';
             ZodDatesObj.innerHTML = 'June 21 - July 22';
             ZodDesObj.innerHTML = 'People often describe you as an outlier. Not because you break rules but because you act as you please. Whether it be through your fashion or your interests, there\’s something about you that breaks people\’s expectations when they get to know you. Some might say you\’re rough around the edges but in actuality you care a lot about those you care about. Even if you are selfish at times, you never want to hurt anyone.';
+            playAudio('KuroAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('KuroAudio');
             break;
         case 'Hello Kitty':
             ZodPicObj.src = 'Media/Hello_Kitty.png';
             ZodDatesObj.innerHTML = 'February 19 - March 20';
             ZodDesObj.innerHTML = 'You are *the* friend to a lot of people. Your friends brag that you are someone they cherish a lot and the one they could never regret being friends with. Often, your closest friends come to you when they need someone to talk to. While you might not be the best at responding in those situations, you are an excellent listener and a trustworthy friend to share problems with.';
+            playAudio('HelloAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('HelloAudio');
             break;
         case 'Pochacco':
             ZodPicObj.src = 'Media/Pochacco.png';
             ZodDatesObj.innerHTML = 'January 20 - February 18';
             ZodDesObj.innerHTML = 'Just like this little dog, you\’re very curious. So curious that you tend to get into other people\’s business unintentionally. Because of that, you often know the best gossip around. As much as you stick your nose where it shouldn\'t be, you are also very clumsy. You even trip over your own feet when walking sometimes. Yet, that doesn\’t deter anyone from trying to be friends with you. Especially because you love to share snacks.';
+            playAudio('PochAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('PochAudio');
             break;
         case 'Little Twin Stars':
             ZodPicObj.src = 'Media/Little_Twin_Stars.png';
             ZodDatesObj.innerHTML = 'May 21 - June 20';
             ZodDesObj.innerHTML = 'Often, there are two sides to your personality. One side that wants to cause chaos and pull pranks. Meanwhile, the other side desires to avoid conflict and keep to oneself. Even when these two sides of you butt heads, they both come together when it comes to protecting those you love. Besides your loved ones, creating things is something you take pride in. Big or small, all the things you create bring joy to you.';
+            playAudio('TwinAudio');
             DisplayZod.classList.remove('hideMe');
             BackgroundDisplay.classList.add('ZodChara');
-            playAudio('TwinAudio');
             break;
 }}
 
